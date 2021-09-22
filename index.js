d5eb6501168016443713f8b078a413ad31aa7f36
ArtemButsev <artbutsev@gmail.com>
2021-09-22 13:19:52 +1000

    validation added

--- index.js
+++ index.js
@@ -1,5 +1,81 @@
+// Question 2
 function validFormFieldInput(data) {
 
 };
+// Question 3
+const newTask = document.querySelector('#newTaskForm');
 
-const newTaskNameInput = document.querySelector('#newTaskNameInput');
+newTask.addEventListener("submit", (event) => {
+const newTaskName = document.querySelector('#newTaskName');
+const newTaskDate = document.querySelector('#newTaskDate');
+const newTaskAssignedTo = document.querySelector('#newTaskAssignedTo');
+const newTaskStatus = document.querySelector('#newTaskStatus');
+const newTaskDescription = document.querySelector('#newTaskDescription');
+let validationFail = 0;
+
+event.preventDefault();
+event.stopPropagation();
+// Question 4
+console.log("name:  " + newTaskName.value.length);
+console.log("Due Date:  " + newTaskDate.value);
+console.log("assignedTo:  " + newTaskAssignedTo.value.length);
+console.log("status:  " + newTaskStatus.value);
+console.log("description:  " + newTaskDescription.value.length);
+
+  // Form validation for Task Name Field min length 5
+  if (newTaskName.value.length > 5) {
+    newTaskName.classList.add("is-valid");
+    newTaskName.classList.remove("is-invalid");
+  } else {
+    newTaskName.classList.add("is-invalid");
+    newTaskName.classList.remove("is-valid");
+    validationFail++;
+  }
+
+  // Form validation for Task Description Field min length 5
+  if (newTaskDescription.value.length > 5) {
+    newTaskDescription.classList.add("is-valid");
+    newTaskDescription.classList.remove("is-invalid");
+  } else {
+    newTaskDescription.classList.add("is-invalid");
+    newTaskDescription.classList.remove("is-valid");
+    validationFail++;
+  }
+
+  // Form validation for Task Assigned Field min length 5
+  if (newTaskAssignedTo.value.length > 5) {
+    newTaskAssignedTo.classList.add("is-valid");
+    newTaskAssignedTo.classList.remove("is-invalid");
+  } else {
+    newTaskAssignedTo.classList.add("is-invalid");
+    newTaskAssignedTo.classList.remove("is-valid");
+    validationFail++;
+  }  
+  // Form validation for Due Date Field not empty
+  // try your own validation for a date in the future
+  if (newTaskDate.value) {
+    newTaskDate.classList.add("is-valid");
+    newTaskDate.classList.remove("is-invalid");
+  } else {
+    newTaskDate.classList.add("is-invalid");
+    newTaskDate.classList.remove("is-valid");
+    validationFail++;
+  }
+  // Form validation for Task Status Field not empty
+  if (newTaskStatus.value) {
+    newTaskStatus.classList.add("is-valid");
+    newTaskStatus.classList.remove("is-invalid");
+  } else {
+    newTaskStatus.classList.add("is-invalid");
+    newTaskStatus.classList.remove("is-valid");
+    validationFail++;
+  }
+  // If validation fails then function will not proceed further and
+  // will return. The value of validationFail will also needed to be
+  // reset to 0.
+  // ----------------------------------------------------------------------------------
+  if (validationFail > 0) {
+    validationFail = 0;
+    return;
+  }
+});
